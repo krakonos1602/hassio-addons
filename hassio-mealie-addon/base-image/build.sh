60ceebe588858c35f77253f790968c7447a98620
krakonos1602 <99399180+krakonos1602@users.noreply.github.com>
2024-04-13 01:20:23 +0200

    Create build.sh

---
+++ hassio-mealie-addon/base-image/build.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+wget https://github.com/mealie-recipes/mealie/archive/refs/tags/v$MEALIE_VERSION.tar.gz
+tar -zxf v$MEALIE_VERSION.tar.gz
+
+export SUB_PATH=nginxsbpth
+
+cd /mealie-$MEALIE_VERSION/frontend && yarn install && yarn generate
+
+docker buildx build --platform linux/aarch64 --tag ghcr.io/krakonos1602/aarch64-hassio-mealie-addon:$MEALIE_VERSION --build-arg MEALIE_VERSION=$MEALIE_VERSION .
