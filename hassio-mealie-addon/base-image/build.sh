188b2ca8686578b5e8fa7c40f69d13ef4a92929b
krakonos1602 <99399180+krakonos1602@users.noreply.github.com>
2024-04-13 17:07:05 +0200

    Update build.sh

--- hassio-mealie-addon/base-image/build.sh
+++ hassio-mealie-addon/base-image/build.sh
@@ -1,11 +1,11 @@
-#!bin/bash
+#!/bin/bash
 
 wget https://github.com/mealie-recipes/mealie/archive/refs/tags/v$MEALIE_VERSION.tar.gz
 tar -zxf v$MEALIE_VERSION.tar.gz
 
 export SUB_PATH=nginxsbpth
 
-cd /mealie-$MEALIE_VERSION/frontend && yarn install --non-interactive --frozen-lockfile && yarn generate
+cd mealie-$MEALIE_VERSION/frontend && yarn install --non-interactive --frozen-lockfile && yarn generate
 
 docker login ghcr.io -u krakonos1602 --password-stdin
 
