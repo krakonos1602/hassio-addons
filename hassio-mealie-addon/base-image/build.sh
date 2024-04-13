de70a267c17e1cb8fd5e960e8677a046678ff84c
krakonos1602 <99399180+krakonos1602@users.noreply.github.com>
2024-04-13 17:11:26 +0200

    Update build.sh

--- hassio-mealie-addon/base-image/build.sh
+++ hassio-mealie-addon/base-image/build.sh
@@ -5,7 +5,9 @@ tar -zxf v$MEALIE_VERSION.tar.gz
 
 export SUB_PATH=nginxsbpth
 
-cd mealie-$MEALIE_VERSION/frontend && yarn install --non-interactive --frozen-lockfile && yarn generate
+cd mealie-$MEALIE_VERSION/frontend && yarn install --non-interactive --frozen-lockfile && yarn generate && cd ../..
+
+echo "Type in GitHub token:"
 
 docker login ghcr.io -u krakonos1602 --password-stdin
 
