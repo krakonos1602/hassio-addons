ae1dbfce6274dd769a240f9865043cc24502a182
Lukáš Červený <cerv.luk@gmail.com>
2024-06-02 02:17:16 +0200

    Updated nginx

--- firefly-iii-addon/rootfs/etc/cont-init.d/nginx.sh
+++ firefly-iii-addon/rootfs/etc/cont-init.d/nginx.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/with-contenv bashio
+set -e
+
 declare ingress_interface
 declare ingress_port
 
@@ -6,3 +8,61 @@ ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+
+# APP_KEY
+APP_KEY="$(bashio::config 'APP_KEY')"
+
+bashio::log.info "Loaded APP_KEY"
+
+# If not base64
+if [[ ! "$APP_KEY" == *"base64"* ]]; then
+    # Check APP_KEY format
+    if [ ! "${#APP_KEY}" = 32 ]; then bashio::exit.nok "Your APP_KEY has ${#APP_KEY} instead of 32 characters"; fi
+fi
+
+# Backup APP_KEY file
+bashio::log.info "Backuping APP_KEY to /config/addons_config/fireflyiii/APP_KEY_BACKUP.txt"
+bashio::log.warning "Changing this value will require to reset your database"
+
+# Get current app_key
+mkdir -p /config/addons_config/fireflyiii
+touch /config/addons_config/fireflyiii/APP_KEY_BACKUP.txt
+CURRENT=$(sed -e '/^[<blank><tab>]*$/d' /config/addons_config/fireflyiii/APP_KEY_BACKUP.txt | sed -n -e '$p')
+
+# Save if new
+if [ "$CURRENT" != "$APP_KEY" ]; then
+    echo "$APP_KEY" >>/config/addons_config/fireflyiii/APP_KEY_BACKUP.txt
+fi
+
+# Update permissions
+mkdir -p /config/addons_config/fireflyiii
+chown -R www-data:www-data /config/addons_config/fireflyiii
+chown -R www-data:www-data /var/www/html/storage
+chmod -R 775 /config/addons_config/fireflyiii
+
+mkdir -p /config/addons_config/fireflyiii/database
+chown -R www-data:www-data /config/addons_config/fireflyiii/database
+
+touch /config/addons_config/fireflyiii/database/database.sqlite
+
+rm -r /var/www/html/storage/database
+ln -s /config/addons_config/fireflyiii/database /var/www/html/storage
+
+mkdir -p /config/addons_config/fireflyiii/upload
+chown -R www-data:www-data /config/addons_config/fireflyiii/upload
+
+rm -r /var/www/html/storage/upload
+ln -s /config/addons_config/fireflyiii/upload /var/www/html/storage
+
+# Updating permissions
+chmod 775 /config/addons_config/fireflyiii/database/database.sqlite
+chown -R www-data:www-data /config/addons_config/fireflyiii
+chown -R www-data:www-data /var/www/html/storage
+
+echo "updating database"
+cd /var/www/html
+
+php artisan firefly-iii:upgrade-database
+php artisan firefly-iii:correct-database
+php artisan firefly-iii:report-integrity
+php artisan firefly-iii:laravel-passport-keys
